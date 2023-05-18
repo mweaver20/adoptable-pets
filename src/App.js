effaf1dffd930973cc2ba0498989c3886245aff6
mweaver20 <morganweaver123@yahoo.com>
2023-05-17 19:02:59 -0700

    added imports

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
 
 // Add react-router-dom imports
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = <Route path="/" element={<Root/>} />;
 
 function App() {
   return (
     // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router={appRouter} />
   );
 }
 
