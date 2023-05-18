6e3bffd975bd79b97928d67fe53c2905e0299c19
mweaver20 <morganweaver123@yahoo.com>
2023-05-17 19:40:49 -0700

    added outlet to root and set homepage

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ import Root from './components/root';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
-const appRouter = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<Root/>} />));
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Root/>}>
+    <Route index element={ <HomePage/>} />
+  </Route>
+));
 
 function App() {
   return (
     // replace below with a Router Provider
-    <RouterProvider router={appRouter} />
+    <RouterProvider path="/" router={appRouter} />
   );
 }
 
